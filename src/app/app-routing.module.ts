b351c73fbe61bc2ec5099890d6758951a5de914a
Maciej Paluch <maciekpal@gmail.com>
2022-10-19 22:43:43 +0200

    feat: added form for creating new product under route ./new-product

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
+import {ProductListComponent} from './components/product-list/product-list.component';
+import {ProductListComponentModule} from './components/product-list/product-list.component-module';
+import {ProductServiceModule} from "./services/product.service-module";
 
 @NgModule({
-  imports: [RouterModule.forRoot([])],
+  imports: [RouterModule.forRoot([{path: 'new-product', component: ProductListComponent}]),ProductServiceModule ,ProductListComponentModule],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+}
---
+++ src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,34 @@
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ProductService } from '../../services/product.service';
+
+@Component({
+  selector: 'app-product-list',
+  styleUrls: ['./product-list.component.scss'],
+  templateUrl: './product-list.component.html',
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ProductListComponent {
+  readonly productForm: FormGroup = new FormGroup({
+    title: new FormControl(),
+    price: new FormControl(),
+    description: new FormControl(),
+    image: new FormControl(),
+    category: new FormControl()
+  });
+
+  constructor(private _productService: ProductService) {
+  }
+
+  onProductFormSubmitted(productForm: FormGroup): void {
+    this._productService.create({
+      category: productForm.get('category')?.value,
+      description: productForm.get('description')?.value,
+      image: productForm.get('image')?.value,
+      price: productForm.get('price')?.value,
+      title: productForm.get('title')?.value
+    }).subscribe();
+
+  }
+}
